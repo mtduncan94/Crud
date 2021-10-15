55322720d604362bb2e9f4b0267c997ba56adfd6
Morris <85505598+mtduncan94@users.noreply.github.com>
2021-10-15 08:32:19 -0400

    Delete RaceAssistant directory

--- RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/entity/RaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raceassistant.RaceAssistant.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RaceRepository extends JpaRepository<RaceDetails, Integer> {
-
-}
