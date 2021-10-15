4f546a78687d9df3d94b9f57fb68be6ce045e00b
Morris <85505598+mtduncan94@users.noreply.github.com>
2021-10-15 13:02:54 -0400

    Add files via upload

--- RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
+++ RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
@@ -35,9 +35,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated().and().formLogin().failureUrl("/login?error=true").permitAll().defaultSuccessUrl("/", true).and().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").invalidateHttpSession(true).permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll().defaultSuccessUrl("/", true).and().logout() .logoutSuccessUrl("/login?logout=true")
+				.invalidateHttpSession(true).permitAll();
 	}
-//.loginPage("/login")
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
