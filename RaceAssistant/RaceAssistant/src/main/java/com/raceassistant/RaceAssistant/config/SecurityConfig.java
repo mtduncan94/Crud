c16f855ebc14c2769288d688f5503f1ae76af0a8
Morris <85505598+mtduncan94@users.noreply.github.com>
2021-10-15 13:08:14 -0400

    Update SecurityConfig.java

--- RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
+++ RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
@@ -36,9 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated().and()
-				.formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll().defaultSuccessUrl("/", true).and().logout() .logoutSuccessUrl("/login?logout=true")
+				.formLogin().failureUrl("/login?error=true").permitAll().defaultSuccessUrl("/", true).and().logout() .logoutSuccessUrl("/login?logout=true")
 				.invalidateHttpSession(true).permitAll();
-	}
+	}//.loginPage("/login")
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
