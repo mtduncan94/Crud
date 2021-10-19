bb0c3c70e3d4c245ab33ba690bb5d53315827db4
Morris <85505598+mtduncan94@users.noreply.github.com>
2021-10-18 23:40:02 -0400

    Add files via upload

--- RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
+++ RaceAssistant/RaceAssistant/src/main/java/com/raceassistant/RaceAssistant/config/SecurityConfig.java
@@ -35,14 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated().and()
-				.formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll().defaultSuccessUrl("/", true).and().logout() .logoutSuccessUrl("/login")
+		http.csrf().disable().authorizeRequests().antMatchers("/admin").hasRole(ADMIN).antMatchers("/user")
+				.hasAnyRole(ADMIN, USER).antMatchers("/login", "/register").permitAll().anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll()
+				.defaultSuccessUrl("/", true).and().logout().logoutSuccessUrl("/login?logout=true")
 				.invalidateHttpSession(true).permitAll();
-	}//.loginPage("/login")
+	}
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 }
